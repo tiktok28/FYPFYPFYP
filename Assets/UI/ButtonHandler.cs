fdb9d6d2a1d3489f7816ee5e3974eae36bf25831
Naveen Ramarao <j.millar458@gmail.com>
2023-10-16 10:42:26 +0800

    Added more buttons and camera switch

--- Assets/UI/ButtonHandler.cs
+++ Assets/UI/ButtonHandler.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
@@ -7,5 +8,24 @@ public class ButtonHandler : MonoBehaviour
     {
         // Your code to execute when the button is clicked
         Debug.Log("Button Clicked!");
+    }
+
+    public void StartButton()
+    {
+        Scene classroom;
+        GameObject menuXR = GameObject.Find("XR Origin (Main Menu)");
+        menuXR.SetActive(false);
+        classroom = SceneManager.GetSceneByName("Classroom");
+        var sceneObjs = classroom.GetRootGameObjects();
+        for (int i = 0; i < sceneObjs.Length; i++){
+            if(sceneObjs[i].name == "XR Origin (Classroom)"){
+                sceneObjs[i].SetActive(true);
+            }
+        }
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
     }
 }
