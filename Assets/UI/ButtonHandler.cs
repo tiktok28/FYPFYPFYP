3a10a513592c51845e508eb12083a85461414237
Naveen Ramarao <j.millar458@gmail.com>
2023-10-24 03:13:51 +0800

    Fixed typewriter effect

--- Assets/UI/ButtonHandler.cs
+++ Assets/UI/ButtonHandler.cs
@@ -35,10 +35,8 @@ public class ButtonHandler : MonoBehaviour
     }
     public void ConceptsButton()
     {
-        SceneManager.LoadScene("Classroom", LoadSceneMode.Additive);
         Scene currentScene = SceneManager.GetActiveScene();
         var currentObjs = currentScene.GetRootGameObjects();
-
         for (int i = 0; i < currentObjs.Length; i++){
             if(currentObjs[i].name == "StartCanvas"){
                 currentObjs[i].SetActive(false);
@@ -51,6 +49,7 @@ public class ButtonHandler : MonoBehaviour
     }
     IEnumerator Concepts()
     {
+        SceneManager.LoadScene("Classroom", LoadSceneMode.Additive);
         yield return new WaitForSeconds(0.1f);
         GameObject mmXR = GameObject.Find("XR Origin (Main Menu)");
         mmXR.SetActive(false);
@@ -62,12 +61,86 @@ public class ButtonHandler : MonoBehaviour
             }
         }
     }
-    public void LightButton()
+    public void DemonstrationsButton()
+    {
+        SceneManager.LoadScene("Classroom", LoadSceneMode.Additive);
+        Scene currentScene = SceneManager.GetActiveScene();
+        var currentObjs = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < currentObjs.Length; i++){
+            if(currentObjs[i].name == "StartCanvas"){
+                currentObjs[i].SetActive(false);
+            }
+            if(currentObjs[i].name == "DemonstrationsCanvas"){
+                currentObjs[i].SetActive(true);
+            }
+        }
+        
+    }
+    IEnumerator Demonstrations()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GameObject mmXR = GameObject.Find("XR Origin (Main Menu)");
+        mmXR.SetActive(false);
+        Scene classroom = SceneManager.GetSceneByName("Classroom");
+        var sceneObjs = classroom.GetRootGameObjects();
+        for (int i = 0; i < sceneObjs.Length; i++){
+            if(sceneObjs[i].name == "XR Origin (Classroom)"){
+                sceneObjs[i].SetActive(true);
+            }
+        }
+    }
+    public void AssessmentsButton()
+    {
+        SceneManager.LoadScene("Classroom", LoadSceneMode.Additive);
+        Scene currentScene = SceneManager.GetActiveScene();
+        var currentObjs = currentScene.GetRootGameObjects();
+
+        for (int i = 0; i < currentObjs.Length; i++){
+            if(currentObjs[i].name == "StartCanvas"){
+                currentObjs[i].SetActive(false);
+            }
+            if(currentObjs[i].name == "AssessmentsCanvas"){
+                currentObjs[i].SetActive(true);
+            }
+        }
+        
+    }
+    IEnumerator Assessments()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GameObject mmXR = GameObject.Find("XR Origin (Main Menu)");
+        mmXR.SetActive(false);
+        Scene classroom = SceneManager.GetSceneByName("Classroom");
+        var sceneObjs = classroom.GetRootGameObjects();
+        for (int i = 0; i < sceneObjs.Length; i++){
+            if(sceneObjs[i].name == "XR Origin (Classroom)"){
+                sceneObjs[i].SetActive(true);
+            }
+        }
+    }
+    public void LightConceptsButton()
     {
         StartCoroutine(Concepts());
     }
-    public void ElectricityButton()
+    public void ElectricityConceptsButton()
     {
         StartCoroutine(Concepts());
+    }
+        public void LightDemonstrationsButton()
+    {
+        StartCoroutine(Demonstrations());
+    }
+    public void ElectricityDemonstrationsButton()
+    {
+        StartCoroutine(Demonstrations());
+    }
+        public void LightAssessmentsButton()
+    {
+        StartCoroutine(Assessments());
+    }
+    public void ElectricityAssessmentsButton()
+    {
+        StartCoroutine(Assessments());
     }
 }
